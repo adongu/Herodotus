c702c952efaa4a9576177887f8b400072f05153c
AlexLiang <liangalex91@gmail.com>
2017-04-28 04:57:25 -0400

    Remove unnecessary module requirement

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -44,13 +44,51 @@ module Associatable
   end
 
   def has_many(name, options = {})
-
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
+    define_method(name) do
+      options = self.class.assoc_options[name]
+      key_val = self.send(options.primary_key)
+      options.model_class.where(options.foreign_key => key_val)
+    end
   end
 
   def assoc_options
+    @assoc_options ||= {}
+  end
 
+  def has_one_through(name, through_name, source_name)
+    define(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through_table = through_options.table_name
+      through_pkey = through_options.primary_key
+      through_fkey = through_options.foreign_key
+
+      source_table = source_options.table_name
+      source_pkey = source_options.primary_key
+      source_fkey = source_options.foreign_key
+      # match method
+      key_val = self.send(through_fkey)
+      # everthing from source table only
+      results = DBConnection.execute(<<-SQL, key_val)
+        SELECT
+          #{source_table}.*
+        FROM
+          #{through_table}
+        JOIN
+          #{source_table}
+        ON
+          #{through_table}.#{source_fkey} = #{source_table}.#{source_pkey}
+        WHERE
+          #{through_table}.#{through_pkey} = ?
+      SQL
+
+      source_options.model_class.parse_all(results).first
+    end
   end
 end
 
 class SQLObject
+  extend Associatable
 end
--- lib/associatable2.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'associatable'
-
-module Associatable
-
-  def has_one_through(name, through_name, source_name)
-  end
-end
